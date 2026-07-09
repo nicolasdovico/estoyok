f15fac7d356f3e1447f471cd5ab682dcc6d055bd
nicolasdovico <nicolasdovico@gmail.com>
2026-07-09 13:50:53 -0300

    fix(android): import BorderStroke in VehiculoScreen.kt

--- android-native/app/src/main/java/com/estoyok/app/features/tracking/presentation/VehiculoScreen.kt
+++ android-native/app/src/main/java/com/estoyok/app/features/tracking/presentation/VehiculoScreen.kt
@@ -1,5 +1,6 @@
 package com.estoyok.app.features.tracking.presentation
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
