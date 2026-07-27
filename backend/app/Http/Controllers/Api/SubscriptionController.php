4ff0c2a15895f462059cf9943948a97056398e8c
nicolasdovico <nicolasdovico@gmail.com>
2026-07-27 11:20:52 -0300

    fix(subscription): import User model in SubscriptionController

--- backend/app/Http/Controllers/Api/SubscriptionController.php
+++ backend/app/Http/Controllers/Api/SubscriptionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Services\MercadoPagoService;
 use App\Services\PayPalService;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
