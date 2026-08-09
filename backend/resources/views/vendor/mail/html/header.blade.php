23da1596bc7da34420519b188af3dec32c1e5e2f
nicolasdovico <nicolasdovico@gmail.com>
2026-08-09 16:11:08 -0300

    correccion en el tamaño del logo

--- backend/resources/views/vendor/mail/html/header.blade.php
+++ backend/resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('images/logo.png') }}" class="logo-app" alt="{{ config('app.name') }}" style="max-height: 55px; width: auto; display: block; margin: 0 auto;">
+<img src="{{ asset('images/logo.png') }}" class="logo-app" alt="{{ config('app.name') }}" style="max-height: 78px; width: auto; display: block; margin: 0 auto;">
 </a>
 </td>
 </tr>
