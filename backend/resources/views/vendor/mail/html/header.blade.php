f6682c6b98d90562b2d8796d25bf3e5ad1afd0db
nicolasdovico <nicolasdovico@gmail.com>
2026-08-12 14:52:51 -0300

    fix(mail): incrustar logo Estoy Ok como Base64 Data URI en header para garantizar su renderizado en railway y clientes de correo

--- backend/resources/views/vendor/mail/html/header.blade.php
+++ backend/resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,14 @@
 @props(['url'])
+@php
+    $logoPath = public_path('images/logo.png');
+    $logoSrc = file_exists($logoPath)
+        ? 'data:image/png;base64,' . base64_encode(file_get_contents($logoPath))
+        : 'https://api.estoyok24.com/images/logo.png';
+@endphp
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('images/logo.png') }}" class="logo-app" alt="{{ config('app.name') }}" style="max-height: 78px; width: auto; display: block; margin: 0 auto;">
+<img src="{{ $logoSrc }}" class="logo-app" alt="{{ config('app.name', 'Estoy Ok') }}" style="max-height: 78px; width: auto; display: block; margin: 0 auto;">
 </a>
 </td>
 </tr>
